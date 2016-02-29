aec32b76b892eb8e001a87e694afa3bf81be7148
Rock3f <badier.tanguy@gmail.com>
2016-02-29 15:40:31 +0100

    Ajout Android Version

---
+++ Liemie_Android/app/src/main/java/com/example/sio/ppe4_kaliemie/Connection_Screen.java
@@ -0,0 +1,57 @@
+package com.example.sio.ppe4_kaliemie;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+
+public class Connection_Screen extends AppCompatActivity {
+
+    private Intent i;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_connection__screen);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        Button btnConnect = (Button) findViewById(R.id.btnConnect);
+        btnConnect.setOnClickListener(btnClick);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_connection__screen, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private View.OnClickListener btnClick = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            i = new Intent(getApplicationContext(), Main_Menu.class);
+            startActivity(i);
+        }
+    };
+}
