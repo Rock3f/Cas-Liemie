6c975c8be0b8d91bacd603b4c56f9c657989ff89
Rock3f <t.badier@hotmail.fr>
2016-04-18 17:23:48 +0200

    PPE 18/04/16
    Modification du menu principal et ajout des différentes pages

--- Liemie_Android/app/src/main/java/com/example/sio/ppe4_kaliemie/Main_Menu.java
+++ Liemie_Android/app/src/main/java/com/example/sio/ppe4_kaliemie/Main_Menu.java
@@ -1,13 +1,18 @@
 package com.example.sio.ppe4_kaliemie;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 public class Main_Menu extends AppCompatActivity {
+
+    private Intent i;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,15 +20,37 @@ public class Main_Menu extends AppCompatActivity {
         setContentView(R.layout.activity_main__menu);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_connection__screen, menu);
+        return true;
+    }
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_Calendrier) {
+            i = new Intent(getApplicationContext(), CalendrierSoin.class);
+            startActivity(i);
+        }
+
+        if (id == R.id.action_Geo) {
+            i = new Intent(getApplicationContext(), Geolocalisation.class);
+            startActivity(i);
+        }
+
+        if (id == R.id.action_ImportExport) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
 }
